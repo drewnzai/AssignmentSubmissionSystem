670047c92a7511b09fd663067cc93a6c7943d9c8
Andrew Nzai <andrewnzaikombe@gmail.com>
2024-03-26 19:53:23 +0300

    Created functionality to getStudentDetails

--- Backend/Student/src/main/java/com/andrew/AssignmentSubmission/services/StudentService.java
+++ Backend/Student/src/main/java/com/andrew/AssignmentSubmission/services/StudentService.java
@@ -1,6 +1,10 @@
 package com.andrew.AssignmentSubmission.services;
 
+import com.andrew.AssignmentSubmission.dto.Student;
+import com.andrew.AssignmentSubmission.exceptions.AssignmentException;
 import com.andrew.AssignmentSubmission.models.User;
+import com.andrew.AssignmentSubmission.repositories.CourseRepository;
+import com.andrew.AssignmentSubmission.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -8,6 +12,26 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class StudentService {
 
-    private CourseService courseService;
+    private UserRepository userRepository;
+
+    public Student getStudentDetails(String registration){
+
+        if(userRepository.existsByRegistration(registration)){
+            User user = userRepository.findByRegistration(registration);
+
+            Student student = new Student();
+
+            student.setRegistration(registration);
+            student.setFullName(user.getFirstName() + user.getLastName());
+            student.setCourseName
+                    (user.getEnrolledCourse().getName());
+
+            return student;
+        }
+        else{
+
+            throw new AssignmentException("No such student exists");
+        }
+    }
 
 }
