a57c3f027691fbc6c35131c0095ca1f962a86885
Andrew Nzai <andrewnzaikombe@gmail.com>
2024-03-26 19:51:10 +0300

    Added new DTO

---
+++ Backend/Student/src/main/java/com/andrew/AssignmentSubmission/dto/Student.java
@@ -0,0 +1,15 @@
+package com.andrew.AssignmentSubmission.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Student {
+
+    private String fullName;
+    private String registration;
+    private String courseName;
+}
