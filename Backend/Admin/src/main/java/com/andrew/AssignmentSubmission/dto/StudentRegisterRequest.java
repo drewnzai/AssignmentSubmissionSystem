2131a28adbc1a0b270eb2f241e9ae82fe1511dd2
Andrew Nzai <andrewnzaikombe@gmail.com>
2024-03-27 21:25:24 +0300

    Added constructors

--- Backend/Admin/src/main/java/com/andrew/AssignmentSubmission/dto/StudentRegisterRequest.java
+++ Backend/Admin/src/main/java/com/andrew/AssignmentSubmission/dto/StudentRegisterRequest.java
@@ -1,8 +1,12 @@
 package com.andrew.AssignmentSubmission.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class StudentRegisterRequest {
     private String firstName;
     private String lastName;
