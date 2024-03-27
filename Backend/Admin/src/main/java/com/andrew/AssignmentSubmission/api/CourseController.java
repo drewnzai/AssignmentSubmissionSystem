39c07cedd7840f4b80a7525f2fab2a4d239d4bb0
Andrew Nzai <andrewnzaikombe@gmail.com>
2024-03-27 22:21:38 +0300

    Commit

--- Backend/Admin/src/main/java/com/andrew/AssignmentSubmission/api/CourseController.java
+++ Backend/Admin/src/main/java/com/andrew/AssignmentSubmission/api/CourseController.java
@@ -1,2 +1,85 @@
-package com.andrew.AssignmentSubmission.api;public class CourseController {
+package com.andrew.AssignmentSubmission.api;
+
+import com.andrew.AssignmentSubmission.dto.CourseDto;
+import com.andrew.AssignmentSubmission.services.CourseService;
+import com.andrew.AssignmentSubmission.utils.APIResponse;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController("/api/course")
+@AllArgsConstructor
+public class CourseController {
+
+    private CourseService courseService;
+
+    @PostMapping
+    public ResponseEntity<APIResponse> addCourse(@RequestBody CourseDto courseDto){
+        if(courseService.addCourse(courseDto.getName())){
+            APIResponse apiResponse = APIResponse.builder()
+                    .message("Course Added Successfully")
+                    .isSuccessful(true)
+                    .statusCode(201)
+                    .build();
+
+            return new ResponseEntity<>(apiResponse, HttpStatus.CREATED);
+        }
+        else {
+
+            APIResponse apiResponse = APIResponse.builder()
+                    .message("The Course Already Exists")
+                    .isSuccessful(false)
+                    .statusCode(409)
+                    .build();
+
+            return new ResponseEntity<>(apiResponse, HttpStatus.CONFLICT);
+        }
+    }
+
+    @DeleteMapping
+    public ResponseEntity<APIResponse> deleteCourse(@RequestBody CourseDto courseDto){
+        if(courseService.deleteCourse(courseDto.getName())){
+
+            APIResponse apiResponse = APIResponse.builder()
+                    .message("Course Deleted Successfully")
+                    .isSuccessful(true)
+                    .statusCode(200)
+                    .build();
+
+            return new ResponseEntity<>(apiResponse, HttpStatus.OK);
+        }
+        else {
+
+            APIResponse apiResponse = APIResponse.builder()
+                    .message("The Course Doesn't Exist")
+                    .isSuccessful(false)
+                    .statusCode(409)
+                    .build();
+
+            return new ResponseEntity<>(apiResponse, HttpStatus.CONFLICT);
+        }
+    }
+
+    @PostMapping("/batch")
+    public ResponseEntity<APIResponse> batch(@RequestBody List<CourseDto> courses){
+
+        for(CourseDto courseDto: courses){
+            courseService.addCourse(courseDto.getName());
+        }
+
+        APIResponse apiResponse = APIResponse.builder()
+                .message("Student Registration Successful")
+                .isSuccessful(true)
+                .statusCode(201)
+                .build();
+
+        return new ResponseEntity<>(apiResponse, HttpStatus.CREATED);
+    }
+
 }
