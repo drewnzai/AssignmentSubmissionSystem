d6dbbdd16a1acd5560e74941fa04f3d1580ba2c9
Andrew Nzai <andrewnzaikombe@gmail.com>
2024-03-27 22:04:51 +0300

    Course managament

---
+++ Backend/Admin/src/main/java/com/andrew/AssignmentSubmission/services/CourseService.java
@@ -0,0 +1,55 @@
+package com.andrew.AssignmentSubmission.services;
+
+import com.andrew.AssignmentSubmission.dto.CourseDto;
+import com.andrew.AssignmentSubmission.exceptions.AssignmentException;
+import com.andrew.AssignmentSubmission.models.Course;
+import com.andrew.AssignmentSubmission.repositories.CourseRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class CourseService {
+
+    private CourseRepository courseRepository;
+
+    public CourseDto getCourseDetails(String courseName){
+
+        Course course = courseRepository.findByName(courseName)
+                .orElseThrow(
+                        () -> new AssignmentException("No Such Course Exists")
+                );
+
+        return new CourseDto(course.getName());
+    }
+
+    public boolean addCourse(String courseName){
+
+        if(courseRepository.existsByName(courseName)){
+            return false;
+        }
+        else{
+
+            Course course = new Course();
+            course.setName(courseName);
+
+            courseRepository.save(course);
+
+            return true;
+        }
+    }
+
+    public boolean deleteCourse(String courseName){
+        if(courseRepository.existsByName(courseName)){
+
+            courseRepository.delete(courseRepository.findByName(courseName).get());
+
+            return true;
+        }
+        else{
+
+            return false;
+        }
+    }
+
+}
