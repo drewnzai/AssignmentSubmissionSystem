1b98b0c7f06ed5256d8e11471738672ba5a75f30
Andrew Nzai <andrewnzaikombe@gmail.com>
2024-03-28 10:04:37 +0300

    Added deleting and modifying functionality

--- Backend/Admin/src/main/java/com/andrew/AssignmentSubmission/services/UnitService.java
+++ Backend/Admin/src/main/java/com/andrew/AssignmentSubmission/services/UnitService.java
@@ -42,4 +42,51 @@ public class UnitService {
             return true;
         }
     }
+
+    public boolean deleteUnit(UnitDto unitDto){
+
+        if(unitRepository.existsByName(unitDto.getName())){
+            Unit unit  = unitRepository.findByName(unitDto.getName());
+
+            unitRepository.delete(unit);
+
+            return true;
+        }
+        else{
+
+            throw new AssignmentException("No such Unit");
+
+            return false;
+        }
+    }
+
+    public boolean modifyUnit(UnitDto unitDto){
+
+        if(unitRepository.existsByName(unitDto.getName())){
+
+            Unit unit  = unitRepository.findByName(unitDto.getName());
+
+            User lecturer = userRepository.findByEmail(unitDto.getLecturerEmail());
+            Semester semester = semesterRepository.findByName(unitDto.getSemester());
+
+            unit.setName(unitDto.getName());
+            unit.setCode(unitDto.getCode());
+            unit.setDescription(unitDto.getDescription());
+            unit.setCredits(unitDto.getCredits());
+            unit.setSemester(semester);
+            unit.setLecturer(lecturer);
+
+            unitRepository.save(unit);
+
+            return true;
+
+        }
+        else{
+
+            throw new AssignmentException("No such Unit");
+
+            return false;
+        }
+
+    }
 }
