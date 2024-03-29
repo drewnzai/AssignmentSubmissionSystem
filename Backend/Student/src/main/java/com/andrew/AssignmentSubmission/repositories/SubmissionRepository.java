e3279bffe454e24db991b69ca90ade9c139b18ba
Andrew Nzai <andrewnzaikombe@gmail.com>
2024-03-29 21:36:16 +0300

    Deleted unused functions

--- Backend/Student/src/main/java/com/andrew/AssignmentSubmission/repositories/SubmissionRepository.java
+++ Backend/Student/src/main/java/com/andrew/AssignmentSubmission/repositories/SubmissionRepository.java
@@ -1,7 +1,7 @@
 package com.andrew.AssignmentSubmission.repositories;
 
+import com.andrew.AssignmentSubmission.dto.Student;
 import com.andrew.AssignmentSubmission.models.Assignment;
-import com.andrew.AssignmentSubmission.models.Student;
 import com.andrew.AssignmentSubmission.models.Submission;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -10,15 +10,4 @@ import java.util.List;
 
 @Repository
 public interface SubmissionRepository extends JpaRepository<Submission, Long> {
-    boolean existsByAssignment(Assignment assignment);
-
-    boolean existsByStudent(Student student);
-
-    boolean existsByAssignmentAndStudent(Assignment assignment, Student student);
-
-    Submission findSubmissionByAssignmentAndStudent(Assignment assignment, Student student);
-
-    List<Submission> findAllByStudent(Student student);
-
-    List<Submission> findAllByAssignment(Assignment assignment);
 }
