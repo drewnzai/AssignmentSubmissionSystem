05e55098af0f63c5e05c3ec112461ef9b00acefd
Andrew Nzai <andrewnzaikombe@gmail.com>
2024-03-29 13:03:14 +0300

    Added submission api

---
+++ Backend/Admin/src/main/java/com/andrew/AssignmentSubmission/api/SubmissionController.java
@@ -0,0 +1,75 @@
+package com.andrew.AssignmentSubmission.api;
+
+import com.andrew.AssignmentSubmission.dto.SubmissionDto;
+import com.andrew.AssignmentSubmission.services.SubmissionService;
+import com.andrew.AssignmentSubmission.utils.APIResponse;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@RestController
+@RequestMapping("/api/submission")
+@AllArgsConstructor
+public class SubmissionController {
+
+    private SubmissionService submissionService;
+
+    @PostMapping
+    public ResponseEntity<APIResponse>
+    submit(@RequestParam("assignmentTitle") String assignmentTitle,
+           @RequestParam("studentRegistration") String studentRegistration,
+           @RequestParam("unitCode") String unitCode,
+           @RequestParam("file") MultipartFile file)
+            throws IOException {
+
+        SubmissionDto submissionDto = new SubmissionDto();
+        submissionDto.setAssignmentTitle(assignmentTitle);
+        submissionDto.setUnitCode(unitCode);
+        submissionDto.setStudentRegistration(studentRegistration);
+
+        if(submissionService.submit(submissionDto, file)){
+            APIResponse apiResponse = APIResponse.builder()
+                    .message("Submitted successfully")
+                    .isSuccessful(true)
+                    .statusCode(201)
+                    .build();
+
+            return new ResponseEntity<>(apiResponse, HttpStatus.CREATED);
+        } else {
+
+            APIResponse apiResponse = APIResponse.builder()
+                    .message("Submission failed")
+                    .isSuccessful(false)
+                    .statusCode(409)
+                    .build();
+
+            return new ResponseEntity<>(apiResponse, HttpStatus.CONFLICT);
+        }
+    }
+
+    @PostMapping("/update")
+    public ResponseEntity<APIResponse> update(@RequestBody SubmissionDto submissionDto){
+        if(submissionService.update(submissionDto)){
+            APIResponse apiResponse = APIResponse.builder()
+                    .message("Submission Updated Successfully")
+                    .isSuccessful(true)
+                    .statusCode(201)
+                    .build();
+
+            return new ResponseEntity<>(apiResponse, HttpStatus.CREATED);
+        } else {
+
+            APIResponse apiResponse = APIResponse.builder()
+                    .message("Submission Does not Exist")
+                    .isSuccessful(false)
+                    .statusCode(409)
+                    .build();
+
+            return new ResponseEntity<>(apiResponse, HttpStatus.CONFLICT);
+        }
+    }
+}
