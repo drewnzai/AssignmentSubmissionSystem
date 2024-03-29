85742a9513a9a85274e6ed0c520e45fe5d7426cd
Andrew Nzai <andrewnzaikombe@gmail.com>
2024-03-29 11:19:44 +0300

    Added submission service

---
+++ Backend/Admin/src/main/java/com/andrew/AssignmentSubmission/services/SubmissionService.java
@@ -0,0 +1,65 @@
+package com.andrew.AssignmentSubmission.services;
+
+import com.andrew.AssignmentSubmission.dto.SubmissionDto;
+import com.andrew.AssignmentSubmission.exceptions.AssignmentException;
+import com.andrew.AssignmentSubmission.models.Assignment;
+import com.andrew.AssignmentSubmission.models.Student;
+import com.andrew.AssignmentSubmission.models.Submission;
+import com.andrew.AssignmentSubmission.repositories.AssignmentRepository;
+import com.andrew.AssignmentSubmission.repositories.StudentRepository;
+import com.andrew.AssignmentSubmission.repositories.SubmissionRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.time.LocalDate;
+
+@Service
+@AllArgsConstructor
+public class SubmissionService {
+
+    private SubmissionRepository submissionRepository;
+    private AssignmentRepository assignmentRepository;
+    private AmazonService amazonService;
+    private StudentRepository studentRepository;
+
+
+    public boolean submit(SubmissionDto submissionDto, MultipartFile multipartFile) throws IOException {
+
+        if(!assignmentRepository.existsByTitle(submissionDto.getAssignmentTitle())
+        || !studentRepository.existsByRegistration(submissionDto.getStudentRegistration())){
+
+            throw new AssignmentException("Assignment or User details are wrong");
+
+        }
+        else if(LocalDate.now().isAfter(
+                assignmentRepository.findByTitle(submissionDto.getAssignmentTitle()).getDue()
+        )){
+              throw new AssignmentException("Assignment is overdue");
+        }
+        else{
+
+            Submission submission = new Submission();
+
+            Assignment assignment = assignmentRepository.findByTitle(submissionDto.getAssignmentTitle());
+            Student student = studentRepository.findByRegistration(submissionDto.getStudentRegistration());
+
+            submission.setAssignment(assignment);
+            submission.setAccepted(false);
+            submission.setScore(0);
+            submission.setStudent(student);
+            submission.setFeedback("");
+            submission.setSubmissionDate(LocalDate.now());
+            submission.setFileName(multipartFile.getOriginalFilename());
+
+            submissionRepository.save(submission);
+
+            amazonService.save(multipartFile, submissionDto.getStudentRegistration(), submissionDto.getUnitCode());
+
+            return true;
+        }
+    }
+
+
+}
