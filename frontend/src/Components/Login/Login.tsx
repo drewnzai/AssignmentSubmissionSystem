e6127e8164dffccfef8e06bd50f067504e0837a0
Andrew Nzai <andrewnzaikombe@gmail.com>
2024-03-30 23:58:48 +0300

    Commits

--- frontend/src/Components/Login/Login.tsx
+++ frontend/src/Components/Login/Login.tsx
@@ -5,21 +5,7 @@ import { FaUser, FaLock } from "react-icons/fa";
 
 function Login(){
   return (
-    <div className='wrapper'>
-        <h1>Login</h1>
-        
-        <div className="input-box">
-          <input type="text" placeholder='Registration Number' required/>
-          <FaUser/>
-        </div>
-        
-        <div className="input-box">
-          <input type="password" placeholder='Password' required/>
-          <FaLock/>
-        </div>
-
-        <button type="submit">Login</button>
-    </div>
+    <div></div>
   );
 }
 
