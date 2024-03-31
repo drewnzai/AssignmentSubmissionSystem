990339c33a3a9aa5e9a1711e510f3efe7cd322cd
Andrew Nzai <andrewnzaikombe@gmail.com>
2024-03-31 11:42:28 +0300

    A lot

--- frontend/src/components/ui/Header/Header.tsx
+++ frontend/src/components/ui/Header/Header.tsx
@@ -4,7 +4,8 @@ import "./Header.css"
 function Header(){
     return(
         <header className='header'>
-            <h1>Egerton Univeristy Assignment Submission System</h1>
+            <img src="/eu-logo.png" alt="" />
+            <h1>Assignment Submission System</h1>
         </header>
     );
 }
