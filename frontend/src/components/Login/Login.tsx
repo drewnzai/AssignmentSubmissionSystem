058519e63328839a1c848106075cc6b8a67cfaa0
Andrew Nzai <andrewnzaikombe@gmail.com>
2024-03-31 11:27:33 +0300

    Login component form created

--- frontend/src/components/Login/Login.tsx
+++ frontend/src/components/Login/Login.tsx
@@ -1,10 +1,45 @@
 import React from 'react';
 import "./Login.css";
+import { LockOutlined, UserOutlined } from '@ant-design/icons';
+import { Button, Form, Input } from 'antd';
 
 
 function Login(){
+
+  const onFinish = (values: any) => {
+    console.log("Received values of form: ", values);
+  };
+
   return (
-    <div>Login</div>
+    <Form
+      name="normal_login"
+      className="login-form"
+      onFinish={onFinish}
+    >
+       <Form.Item
+        name="registration"
+        rules={[{ required: true, message: 'Please input your Registration Number!' }]}
+      >
+        <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Registration Number" />
+      </Form.Item>
+
+      <Form.Item
+        name="password"
+        rules={[{ required: true, message: 'Please input your Password!' }]}
+      >
+        <Input
+          prefix={<LockOutlined className="site-form-item-icon" />}
+          type="password"
+          placeholder="Password"
+        />
+      </Form.Item>
+
+      <Form.Item>
+        <Button type="primary" htmlType="submit" className="login-form-button">
+          Log in
+        </Button>
+      </Form.Item>
+     </Form>
   );
 }
 
