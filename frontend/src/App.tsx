99212d17ed6fa7cdd121ecabbb739285bb9bfb08
Andrew Nzai <andrewnzaikombe@gmail.com>
2024-03-31 10:05:26 +0300

    Renamed to 'components'

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
-import Login from "./Components/Login/Login"
+import Login from "./components/Login/Login"
 
 const App: React.FC = () => (
 	<BrowserRouter>
