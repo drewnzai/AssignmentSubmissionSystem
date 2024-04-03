0390da0adf49ceb0e1b9381d671b4ebc755c730d
Andrew Nzai <andrewnzaikombe@gmail.com>
2024-04-03 22:25:53 +0300

    Starting all over

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,11 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
-import Header from "./components/ui/Header/Header";
-import Login from "./components/Login/Login";
 
 const App: React.FC = () => (
 	<BrowserRouter>
 		<Routes>
-			<Route path="/login" Component={() => {
-        return(
-          <>
-          <div className="imageFill">
-          <div className="egerton">
-          <Header/>
-        
-          <Login/>
-        
-          </div>
-          </div>
-          </>
-        );
-      }} />
+     
 		</Routes>
 	</BrowserRouter>
 );
