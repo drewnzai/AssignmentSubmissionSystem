abcc2323ed50880d2159e77cbabf83846538b279
Andrew Nzai <andrewnzaikombe@gmail.com>
2024-04-04 02:31:34 +0300

    Renamed to sidebarImpl

---
+++ frontend/src/components/Sidebar/SidebarImpl.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./SidebarImpl.css";
+import 'react-pro-sidebar/dist/css/styles.css';
+
+import { Sidebar, Menu, MenuItem, SubMenu } from 'react-pro-sidebar';
+
+function SidebarImpl(){
+    return (
+        <></>
+      );
+}
+
+export default SidebarImpl;
