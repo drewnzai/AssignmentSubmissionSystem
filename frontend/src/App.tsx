23ccfae52aa3b6d2c9e9267328c04af46c830195
Andrew Nzai <andrewnzaikombe@gmail.com>
2024-04-03 23:13:25 +0300

    Added path

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
+import Login from './components/Login/Login';
 
 const App: React.FC = () => (
 	<BrowserRouter>
 		<Routes>
-     
+     <Route path='/' Component={Login}/>
 		</Routes>
 	</BrowserRouter>
 );
