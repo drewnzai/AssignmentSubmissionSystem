7aba1cce1b5a068bdb0b91ce0b8f02b9c53b69b5
Andrew Nzai <andrewnzaikombe@gmail.com>
2024-04-04 02:48:01 +0300

    Added links

--- frontend/src/components/Sidebar/SidebarImpl.tsx
+++ frontend/src/components/Sidebar/SidebarImpl.tsx
@@ -3,10 +3,22 @@ import "./SidebarImpl.css";
 import 'react-pro-sidebar/dist/css/styles.css';
 
 import { Sidebar, Menu, MenuItem, SubMenu } from 'react-pro-sidebar';
+import { Link } from "react-router-dom";
 
 function SidebarImpl(){
     return (
-        <></>
+      <Sidebar>
+      <Menu>
+        <SubMenu title="Assignments">
+        <MenuItem component={<Link to="/assignments/pending" />}> Pending</MenuItem>
+        <MenuItem component={<Link to="/assignments/completed" />}> Completed</MenuItem>
+        </SubMenu>
+        <MenuItem>
+          Students
+          <Link to="/students" />
+        </MenuItem>
+      </Menu>
+    </Sidebar>
       );
 }
 
