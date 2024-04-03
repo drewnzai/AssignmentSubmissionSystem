ed5ab25835b5d138026ad236a1b2ed19adfdcdb0
Andrew Nzai <andrewnzaikombe@gmail.com>
2024-04-03 23:16:32 +0300

    Changed paths

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Login from './components/Login/Login';
+import Home from './components/Home/Home';
 
 const App: React.FC = () => (
 	<BrowserRouter>
 		<Routes>
-     <Route path='/' Component={Login}/>
+     <Route path='/' Component={Home}/>
+     <Route path='/login' Component={Login}/>
 		</Routes>
 	</BrowserRouter>
 );
