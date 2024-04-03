935c6513e20a721937d47bf0e1e80c62cdcce842
Andrew Nzai <andrewnzaikombe@gmail.com>
2024-04-04 01:38:30 +0300

    Modified the login page

--- frontend/src/components/Home/Home.tsx
+++ frontend/src/components/Home/Home.tsx
@@ -3,29 +3,10 @@ import { useNavigate } from "react-router-dom";
 import "./Home.css";
 
 function Home(props: any){
-  const { loggedIn, email } = props;
-  const navigate = useNavigate();
-
-  const onButtonClick = () => {
-    // You'll update this function later
-  };
+  
 
   return (
-    <div className="mainContainer">
-      <div className={'titleContainer'}>
-        <div>Welcome!</div>
-      </div>
-      <div>This is the home page.</div>
-      <div className={'buttonContainer'}>
-        <input
-          className={'inputButton'}
-          type="button"
-          onClick={onButtonClick}
-          value={loggedIn ? 'Log out' : 'Log in'}
-        />
-        {loggedIn ? <div>Your email address is {email}</div> : <div />}
-      </div>
-    </div>
+    <></>
   );
 
   
