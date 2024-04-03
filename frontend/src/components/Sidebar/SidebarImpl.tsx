239b39f8c3253740a19aefd7ebb19e9d2e3479df
Andrew Nzai <andrewnzaikombe@gmail.com>
2024-04-04 02:53:21 +0300

    Added a book icon

--- frontend/src/components/Sidebar/SidebarImpl.tsx
+++ frontend/src/components/Sidebar/SidebarImpl.tsx
@@ -4,13 +4,14 @@ import 'react-pro-sidebar/dist/css/styles.css';
 
 import { Sidebar, Menu, MenuItem, SubMenu } from 'react-pro-sidebar';
 import { Link } from "react-router-dom";
+import BookIcon from '@mui/icons-material/Book';
 
 function SidebarImpl(){
     return (
       <Sidebar>
       <Menu>
       <MenuItem component={<Link to="/student" />}> Student Details</MenuItem>
-        <SubMenu title="Assignments">
+        <SubMenu icon="BookIcon" title="Assignments">
         <MenuItem component={<Link to="/assignments/pending" />}> Pending</MenuItem>
         <MenuItem component={<Link to="/assignments/completed" />}> Completed</MenuItem>
         </SubMenu>
