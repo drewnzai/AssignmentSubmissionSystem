5db557d9c4f38df6d0f968a828230925db2c80ae
Andrew Nzai <andrewnzaikombe@gmail.com>
2024-04-04 14:36:01 +0300

    Working on the component

--- frontend/src/components/Student/Student.tsx
+++ frontend/src/components/Student/Student.tsx
@@ -1,9 +1,28 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import "./Student.css";
+import SidebarImpl from "../Sidebar/SidebarImpl";
+import authService from "../../auth/auth.service";
 
 function Student(){
+
+    const navigate = useNavigate();
+
+    useEffect((
+        () => {
+            const currentUser = authService.getCurrentUser();
+
+            if(!currentUser){
+                navigate("/login");
+              }
+        }
+    ), [navigate]);
+
     return(
-        <></>
+        <div style={{display: "flex"}}>
+            <SidebarImpl/>
+            <p>details</p>
+        </div>
     );
 }
 
