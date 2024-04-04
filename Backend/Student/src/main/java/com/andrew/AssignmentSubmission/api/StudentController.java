d3b653d151e2634c3b1ffb66b1fa5f2992851a76
Andrew Nzai <andrewnzaikombe@gmail.com>
2024-04-04 15:27:18 +0300

    Fixed mapping

--- Backend/Student/src/main/java/com/andrew/AssignmentSubmission/api/StudentController.java
+++ Backend/Student/src/main/java/com/andrew/AssignmentSubmission/api/StudentController.java
@@ -4,19 +4,18 @@ import com.andrew.AssignmentSubmission.dto.Student;
 import com.andrew.AssignmentSubmission.services.StudentService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-@RestController("/api/student/")
+@RestController
+@RequestMapping("/api/student/")
 @RequiredArgsConstructor
 @Tag(name = "Student", description = "Student Details APIs")
 public class StudentController {
 
     private StudentService studentService;
 
-    @GetMapping("{registration}")
-    public Student getDetails(@PathVariable String registration){
+    @GetMapping()
+    public Student getDetails(@RequestBody String registration){
         return studentService.getStudentDetails(registration);
     }
 
