a276b26519822b5dd12f4b9f78bcf66f40a7e793
Andrew Nzai <andrewnzaikombe@gmail.com>
2024-04-04 10:00:14 +0300

    Added logout link

--- frontend/src/components/Sidebar/SidebarImpl.tsx
+++ frontend/src/components/Sidebar/SidebarImpl.tsx
@@ -4,9 +4,7 @@ import "./SidebarImpl.css";
 import { Sidebar, Menu, MenuItem, SubMenu } from 'react-pro-sidebar';
 import { Link } from "react-router-dom";
 import BookIcon from '@mui/icons-material/Book';
-import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
-import PeopleOutlinedIcon from "@mui/icons-material/PeopleOutlined";
-import ContactsOutlinedIcon from "@mui/icons-material/ContactsOutlined";
+import LogoutIcon from '@mui/icons-material/Logout';
 import ReceiptOutlinedIcon from "@mui/icons-material/ReceiptOutlined";
 import CalendarTodayOutlinedIcon from "@mui/icons-material/CalendarTodayOutlined";
 import HelpOutlineOutlinedIcon from "@mui/icons-material/HelpOutlineOutlined";
@@ -32,9 +30,7 @@ function SidebarImpl(){
         <MenuItem component={<Link to="/assignments/pending" />}> Pending</MenuItem>
         <MenuItem component={<Link to="/assignments/completed" />}> Completed</MenuItem>
         </SubMenu>
-        <MenuItem icon={<HomeOutlinedIcon />}>Home</MenuItem>
-          <MenuItem icon={<PeopleOutlinedIcon />}>Team</MenuItem>
-          <MenuItem icon={<ContactsOutlinedIcon />}>Contacts</MenuItem>
+          <MenuItem icon={<LogoutIcon/> } component={<Link to="/logout" />}>Logout</MenuItem>
           <MenuItem icon={<HelpOutlineOutlinedIcon />}>FAQ</MenuItem>
           <MenuItem icon={<CalendarTodayOutlinedIcon />}>Calendar</MenuItem>
       </Menu>
