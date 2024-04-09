4e373dbf3c8cae65256b0ca4e3b9183302a880ca
Andrew Nzai <andrewnzaikombe@gmail.com>
2024-04-09 23:50:46 +0300

    Created a unit service

---
+++ frontend/src/services/unit.service.ts
@@ -0,0 +1,11 @@
+import axios from "axios";
+import authHeader from "../auth/auth.header";
+
+const API_URL = "http://localhost:8080/api/unit";
+
+export default function UnitService(){
+
+    
+
+
+}
