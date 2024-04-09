8740eaecf6be5bcdfe3775c35b8e3a588c5d0498
Andrew Nzai <andrewnzaikombe@gmail.com>
2024-04-10 00:01:04 +0300

    Renamed authHeader  to AuthHeader

--- frontend/src/services/unit.service.ts
+++ frontend/src/services/unit.service.ts
@@ -1,11 +1,12 @@
 import axios from "axios";
 import authHeader from "../auth/auth.header";
+import AuthService from "./auth.service";
 
 const API_URL = "http://localhost:8080/api/unit";
 
 export default function UnitService(){
 
-    
+    let userDetails = AuthService.getCurrentUser();
 
 
 }
