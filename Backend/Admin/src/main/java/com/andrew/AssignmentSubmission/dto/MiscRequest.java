3720ffa684ec192ccc851a42a83ee9a00b17237c
Andrew Nzai <andrewnzaikombe@gmail.com>
2024-04-10 08:30:31 +0300

    Created a miscellaneous request DTO

---
+++ Backend/Admin/src/main/java/com/andrew/AssignmentSubmission/dto/MiscRequest.java
@@ -0,0 +1,13 @@
+package com.andrew.AssignmentSubmission.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class MiscRequest {
+
+    private String data;
+}
