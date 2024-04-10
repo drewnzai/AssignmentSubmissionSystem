fabb5a1d369d94a328736cb107a7449130cce3fb
Andrew Nzai <andrewnzaikombe@gmail.com>
2024-04-10 08:25:42 +0300

    Changed mapping, changed to miscRequest requestbody

--- Backend/Student/src/main/java/com/andrew/AssignmentSubmission/api/UnitController.java
+++ Backend/Student/src/main/java/com/andrew/AssignmentSubmission/api/UnitController.java
@@ -1,13 +1,11 @@
 package com.andrew.AssignmentSubmission.api;
 
+import com.andrew.AssignmentSubmission.dto.MiscRequest;
 import com.andrew.AssignmentSubmission.dto.UnitDto;
 import com.andrew.AssignmentSubmission.services.UnitService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,9 +17,9 @@ public class UnitController {
 
     private UnitService unitService;
 
-    @GetMapping("/{courseName}")
-    public List<UnitDto> getUnitsFromCourse(@PathVariable String courseName){
-        return unitService.getCurrentUnitsFromCourse(courseName);
+    @PostMapping()
+    public List<UnitDto> getUnitsFromCourse(@RequestBody MiscRequest miscRequest){
+        return unitService.getCurrentUnitsFromCourse(miscRequest.getData());
     }
 
 }
