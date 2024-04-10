11ef8893da90f319d2285a1c613ccfa51a0b2208
Andrew Nzai <andrewnzaikombe@gmail.com>
2024-04-10 12:35:15 +0300

    Added TO-DO

--- frontend/src/services/Assignment.service.ts
+++ frontend/src/services/Assignment.service.ts
@@ -1,4 +1,14 @@
+import AuthService from "./Auth.service";
 
 export default class AssignmentService{
-    
+ 
+    getPendingAssignments(){
+        
+        const authService = new AuthService();
+
+        let user = authService.getCurrentUser();
+
+        //TO-DO maybe store user units in current context
+
+    }
 }
