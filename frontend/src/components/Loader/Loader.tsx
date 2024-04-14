2286898fdbb6a629fcb97088178b4699365cc068
Andrew Nzai <andrewnzaikombe@gmail.com>
2024-04-14 17:57:43 +0300

    Created new loader

---
+++ frontend/src/components/Loader/Loader.tsx
@@ -0,0 +1,12 @@
+import { ClimbingBoxLoader } from "react-spinners";
+
+
+function Loader(){
+    return(
+        <div>
+            <ClimbingBoxLoader color="rgba(12, 237, 41, 1)" />
+        </div>
+    );
+}
+
+export default Loader;
