d6395910b4127c1f0c2a7bbac8723d459d0eca27
Andrew Nzai <andrewnzaikombe@gmail.com>
2024-04-14 20:05:10 +0300

    Created functionality to return assignments from all units by course

--- Backend/Student/src/main/java/com/andrew/AssignmentSubmission/api/AssignmentController.java
+++ Backend/Student/src/main/java/com/andrew/AssignmentSubmission/api/AssignmentController.java
@@ -2,11 +2,13 @@ package com.andrew.AssignmentSubmission.api;
 
 import com.andrew.AssignmentSubmission.dto.AssignmentDto;
 import com.andrew.AssignmentSubmission.dto.MiscRequest;
+import com.andrew.AssignmentSubmission.dto.UnitList;
 import com.andrew.AssignmentSubmission.services.AssignmentService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -17,7 +19,8 @@ public class AssignmentController {
 
     private AssignmentService assignmentService;
     @PostMapping()
-    public List<AssignmentDto> getPendingAssignmentsByUnit(@RequestBody MiscRequest miscRequest){
-        return assignmentService.pendingAssignmentsByUnit(miscRequest.getData());
+    public List<AssignmentDto> getPendingAssignmentsByUnit(@RequestBody UnitList unitList){
+
+        return assignmentService.pendingAssignmentsByUnit(unitList.getData());
     }
 }
--- Backend/Student/src/main/java/com/andrew/AssignmentSubmission/services/AssignmentService.java
+++ Backend/Student/src/main/java/com/andrew/AssignmentSubmission/services/AssignmentService.java
@@ -18,29 +18,33 @@ public class AssignmentService {
 
     private AssignmentRepository assignmentRepository;
     private UnitRepository unitRepository;
-    public List<AssignmentDto> pendingAssignmentsByUnit(String unitCode){
-
-        Unit unit = unitRepository.findByCode(unitCode);
-
-        List<Assignment> assignments = assignmentRepository.findAllByUnit(unit);
+    public List<AssignmentDto> pendingAssignmentsByUnit(List<String> units) {
 
         List<AssignmentDto> pendingAssignments = new ArrayList<>();
 
-        for(Assignment assignment: assignments){
+        for (String unitCode : units) {
 
-            if(LocalDate.now().isBefore(assignment.getDue())){
+            Unit unit = unitRepository.findByCode(unitCode);
 
-                AssignmentDto assignmentDto = new AssignmentDto();
+            List<Assignment> assignments = assignmentRepository.findAllByUnit(unit);
 
-                assignmentDto.setUnitCode(unitCode);
-                assignmentDto.setTitle(assignment.getTitle());
-                assignmentDto.setDescription(assignment.getDescription());
-                assignmentDto.setDue(assignment.getDue());
-                assignmentDto.setLecturerEmail(assignment.getLecturer().getEmail());
+            for (Assignment assignment : assignments) {
 
-                pendingAssignments.add(assignmentDto);
+                if (LocalDate.now().isBefore(assignment.getDue())) {
 
+                    AssignmentDto assignmentDto = new AssignmentDto();
+
+                    assignmentDto.setUnitCode(unitCode);
+                    assignmentDto.setTitle(assignment.getTitle());
+                    assignmentDto.setDescription(assignment.getDescription());
+                    assignmentDto.setDue(assignment.getDue());
+                    assignmentDto.setLecturerEmail(assignment.getLecturer().getEmail());
+
+                    pendingAssignments.add(assignmentDto);
+
+                }
             }
+
         }
         return pendingAssignments;
     }
