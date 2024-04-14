8776a2652a0778d50037489721eee3bca8e21bd5
Andrew Nzai <andrewnzaikombe@gmail.com>
2024-04-14 19:53:32 +0300

    Created a DTO to handle a list of unit names as request

---
+++ Backend/Student/src/main/java/com/andrew/AssignmentSubmission/dto/UnitList.java
@@ -0,0 +1,15 @@
+package com.andrew.AssignmentSubmission.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UnitList {
+
+    private List<String> data;
+}
