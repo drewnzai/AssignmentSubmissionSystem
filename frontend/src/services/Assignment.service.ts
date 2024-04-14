732f5a77c1faf3ec95b32e23fc3f0ae66e23434f
Andrew Nzai <andrewnzaikombe@gmail.com>
2024-04-14 19:02:10 +0300

    Implemented Pending assignments function

--- frontend/src/services/Assignment.service.ts
+++ frontend/src/services/Assignment.service.ts
@@ -1,14 +1,36 @@
-import AuthService from "./Auth.service";
+import axios from "axios";
+import AuthHeader from "../auth/Auth.header";
+import { MiscRequest } from "../models/MiscRequest";
+import { Unit } from "../models/Unit";
+import { useState } from "react";
+import { Assignment } from "../models/Assignment";
+
+const API_URL = "http://localhost:8080/api/assignment";
+
+const units:Unit[] = JSON.parse(localStorage.getItem("units")!);
+
 
 export default class AssignmentService{
- 
+
     getPendingAssignments(){
-        
-        const authService = new AuthService();
+    
+    const [assignments, setAssignments] = useState<Assignment[]>([]);
+    
+    const unit = units[1];
 
-        let user = authService.getCurrentUser();
-
-        //TO-DO implement this List<AssignmentDto> pendingAssignmentsByUnit(String unitCode)
-        
+    const miscRequest: MiscRequest = {
+        data: unit.code
     }
+    
+    //TO-DO implement this List<AssignmentDto> pendingAssignmentsByUnit(String unitCode)
+    return axios.post(API_URL, miscRequest, {headers: AuthHeader()})
+    .then(
+        (response) => {
+            if(response.data){
+                setAssignments(response.data);
+            }
+            return assignments;
+        }
+    );
+}
 }
