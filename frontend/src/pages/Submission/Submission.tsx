0f55d938f735ed8453846a8dfb44e8228f09bc17
Andrew Nzai <andrewnzaikombe@gmail.com>
2024-04-18 16:26:40 +0300

    Added submission functionlaity

--- frontend/src/pages/Submission/Submission.tsx
+++ frontend/src/pages/Submission/Submission.tsx
@@ -1,16 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Submission.css";
 import { useLocation } from "react-router-dom";
+import AuthService from "../../services/Auth.service";
+import AuthHeader from "../../auth/Auth.header";
+import axios from "axios";
+
+const API_URL = "http://localhost:8080/api/submit";
 
 function Submission(){
 
     const location = useLocation();
     const {assignment} = location.state;
+    const [file, setFile] = useState<File | null>(null);
+    const authService = new AuthService();
+
+    const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        if (event.target.files) {
+            setFile(event.target.files[0]);
+        }
+    };
+
+    const handleSubmit = async (event: React.FormEvent) => {
+        event.preventDefault();
+        
+        if (!file) {
+            alert('Please select a file');
+            return;
+        }
+
+        const user = authService.getCurrentUser();
+
+        const formData = new FormData();
+        formData.append('assignmentTitle', assignment.title);
+        formData.append('studentRegistration', user.registration);
+        formData.append('unitCode', assignment.unitCode);
+        formData.append('file', file);
+
+        try{
+            const response = await axios.post(API_URL, formData, {headers: AuthHeader()});
+            
+            console.log(response.data);            
+            alert('File submitted successfully');
+
+        } catch (error) {
+            
+            console.error('Error submitting the form', error);
+
+            alert('Failed to submit the file');
+        }
+    };
 
     return(
         <div>
-            <p>{assignment.title}</p>
-            <p>{assignment.description}</p>
+            <form onSubmit={handleSubmit}>
+            <label>
+                File:
+                <input type="file" onChange={handleFileChange} />
+            </label>
+            <br />
+            <button type="submit">Submit</button>
+            </form>
         </div>
     );
 }
