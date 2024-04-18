f5e7a411c7f9ddb699db07f89e1df6e95faae255
Andrew Nzai <andrewnzaikombe@gmail.com>
2024-04-18 15:28:51 +0300

    Changed loader

--- frontend/src/components/Loader/Loader.tsx
+++ frontend/src/components/Loader/Loader.tsx
@@ -1,8 +1,18 @@
+import { Grid } from "react-loader-spinner";
 
 function Loader(){
     return(
         <div>
-            
+        <Grid 
+        visible={true}
+        height="100"
+        width="100"
+        color="#4fa94d"
+        ariaLabel="grid-loading"
+        radius="13.5"
+        wrapperStyle={{}}
+        wrapperClass="grid-wrapper"
+  />            
         </div>
     );
 }
