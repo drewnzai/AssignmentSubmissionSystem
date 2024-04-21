3e5a7f64ead08bf1354a3460db2a9b470adf80ce
Andrew Nzai <andrewnzaikombe@gmail.com>
2024-04-21 16:54:31 +0300

    Added populate method

--- Backend/Admin/src/main/java/com/andrew/AssignmentSubmission/services/PendingService.java
+++ Backend/Admin/src/main/java/com/andrew/AssignmentSubmission/services/PendingService.java
@@ -1,5 +1,6 @@
 package com.andrew.AssignmentSubmission.services;
 
+import com.andrew.AssignmentSubmission.models.Assignment;
 import com.andrew.AssignmentSubmission.repositories.AssignmentRepository;
 import com.andrew.AssignmentSubmission.repositories.PendingRepository;
 import lombok.AllArgsConstructor;
@@ -12,6 +13,8 @@ public class PendingService {
     private AssignmentRepository assignmentRepository;
     private PendingRepository pendingRepository;
 
+    public void populate(Assignment assignment){
 
+    }
 
 }
