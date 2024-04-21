2a7e1e713c7acefe1a87838a2fde4e268575bcdf
Andrew Nzai <andrewnzaikombe@gmail.com>
2024-04-21 16:47:28 +0300

    Added Pending assignments table

---
+++ Backend/Student/src/main/java/com/andrew/AssignmentSubmission/models/Pending.java
@@ -0,0 +1,36 @@
+package com.andrew.AssignmentSubmission.models;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Data
+@Table(name = "pending")
+@AllArgsConstructor
+@NoArgsConstructor
+public class Pending {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long assignmentId;
+    private String title;
+    private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "unit_id")
+    private Unit unit;
+
+    @ManyToOne
+    @JoinColumn(name = "student_id")
+    private User user;
+
+    @JsonFormat(pattern = "dd/MM/yyyy")
+    private LocalDate due;
+
+
+}
