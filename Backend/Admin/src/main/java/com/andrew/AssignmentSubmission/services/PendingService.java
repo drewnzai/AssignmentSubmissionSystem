a3389aac24a36eb5abcefaaa40c068b783d80f5b
Andrew Nzai <andrewnzaikombe@gmail.com>
2024-04-21 17:16:50 +0300

    Finished populate method

--- Backend/Admin/src/main/java/com/andrew/AssignmentSubmission/services/PendingService.java
+++ Backend/Admin/src/main/java/com/andrew/AssignmentSubmission/services/PendingService.java
@@ -1,10 +1,12 @@
 package com.andrew.AssignmentSubmission.services;
 
-import com.andrew.AssignmentSubmission.models.Assignment;
-import com.andrew.AssignmentSubmission.repositories.AssignmentRepository;
-import com.andrew.AssignmentSubmission.repositories.PendingRepository;
+import com.andrew.AssignmentSubmission.models.*;
+import com.andrew.AssignmentSubmission.repositories.*;
 import lombok.AllArgsConstructor;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @AllArgsConstructor
@@ -12,9 +14,33 @@ public class PendingService {
 
     private AssignmentRepository assignmentRepository;
     private PendingRepository pendingRepository;
+    private CourseRepository courseRepository;
+    private OfferingRepository offeringRepository;
+    private StudentRepository studentRepository;
 
+    @Async
     public void populate(Assignment assignment){
 
+        Course course = offeringRepository
+                .findByUnit(assignment.getUnit()).getCourse();
+
+        List<Student> students = studentRepository.findAllByEnrolledCourse(course);
+
+        for (Student student: students){
+
+            Pending pending = new Pending();
+
+            pending.setDue(assignment.getDue());
+            pending.setDescription(assignment.getDescription());
+            pending.setTitle(assignment.getTitle());
+            pending.setUnit(assignment.getUnit());
+            pending.setStudent(student);
+
+            pendingRepository.save(pending);
+        }
+
     }
+
+
 
 }
