9cbb8637aee77356edf3ded75cacf6877a383444
Andrew Nzai <andrewnzaikombe@gmail.com>
2024-04-22 09:00:14 +0300

    New general assignment repository

---
+++ Backend/Student/src/main/java/com/andrew/AssignmentSubmission/repositories/AssignmentRepository.java
@@ -0,0 +1,9 @@
+package com.andrew.AssignmentSubmission.repositories;
+
+import com.andrew.AssignmentSubmission.models.Assignment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AssignmentRepository extends JpaRepository<Assignment, Long> {
+}
