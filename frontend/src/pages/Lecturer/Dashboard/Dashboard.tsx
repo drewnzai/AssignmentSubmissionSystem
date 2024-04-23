ac5c2f97458f53cba9aee1f57bc8879f0e140042
Andrew Nzai <andrewnzaikombe@gmail.com>
2024-04-24 01:19:38 +0300

    Get units

--- frontend/src/pages/Lecturer/Dashboard/Dashboard.tsx
+++ frontend/src/pages/Lecturer/Dashboard/Dashboard.tsx
@@ -3,10 +3,17 @@ import { useNavigate } from "react-router-dom";
 import LecAuthService from "../../../services/LecAuth.service";
 import LecSidebar from "../../../components/Sidebar/LecSidebar";
 import { Unit } from "../../../models/Unit";
+import LecUnitService from "../../../services/LecUnit.service";
+import Loader from "../../../components/Loader/Loader";
+import "./Dashboard.css";
 
 function LecDashboard(){
     const authService = new LecAuthService();
+    const unitService = new LecUnitService();
+    
     const [units, setUnits] = useState<Unit[]>([]);
+    const [loading, setLoading] = useState(true);
+  
     
     const navigate = useNavigate();
     
@@ -17,7 +24,27 @@ function LecDashboard(){
         navigate("/login");
       }
 
+      setTimeout(
+        () => {
+          unitService.getAssignedUnits()
+          .then(
+            (response) => {
+              console.log(response);
+              setLoading(false);
+            }
+          );
+        }
+      , 3000);
+
     }, [navigate]);
+
+    if(loading){
+      return(
+        <div className="container">
+          <Loader/>
+        </div>
+      );
+    }
 
     return(
         <div>
