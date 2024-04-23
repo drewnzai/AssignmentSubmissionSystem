8ed33366d03b083346b0116776b7463c6e475c74
Andrew Nzai <andrewnzaikombe@gmail.com>
2024-04-23 20:17:38 +0300

    Lecturer Login

---
+++ frontend/src/services/LecAuth.service.ts
@@ -0,0 +1,33 @@
+import axios from "axios";
+import { UserLoginRequest } from "../models/UserLoginRequest";
+
+const API_URL = "http://localhost:8080/api/auth/"
+
+export default class LecAuthService{
+    login(loginRequest: UserLoginRequest) {
+    
+        return axios
+          .post(API_URL + "login", loginRequest)
+          .then(response => {
+            if (response.data.authenticationToken) {
+              localStorage.setItem("lecturer", JSON.stringify(response.data));
+            }
+    
+            return response.data;
+          });
+      }
+    
+      logout() {
+        localStorage.removeItem("lecturer");
+        localStorage.removeItem("units");
+      }
+    
+    
+      getCurrentUser() {
+        const userStr = localStorage.getItem("lecturer");
+        if (userStr) return JSON.parse(userStr);
+    
+        return null;
+      }
+    
+}
