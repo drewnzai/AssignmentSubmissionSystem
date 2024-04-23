e30ca6f043d705a13b9eb1db4ed82ced868055e4
Andrew Nzai <andrewnzaikombe@gmail.com>
2024-04-23 21:02:43 +0300

    Created Admin Dasboard

--- frontend/src/pages/Admin/Dashboard/Dashboard.tsx
+++ frontend/src/pages/Admin/Dashboard/Dashboard.tsx
@@ -1,4 +1,26 @@
+import { useEffect } from "react";
+import AdAuthService from "../../../services/AdAuth.service";
+import { useNavigate } from "react-router-dom";
+import AdSidebar from "../../../components/Sidebar/AdSidebar";
+
 function AdDashboard(){
+    const authService = new AdAuthService();
+    const navigate = useNavigate();
+    
+    useEffect(() => {
+        const currentUser = authService.getCurrentUser();
+      
+      if(!currentUser){
+        navigate("/login");
+      }
+
+    }, []);
+
+    return (
+        <div>
+        <AdSidebar/>
+        </div>
+      );
     
 }
 
