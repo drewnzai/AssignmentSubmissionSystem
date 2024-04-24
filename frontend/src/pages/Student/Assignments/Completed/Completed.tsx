d663b92f02467c441c451e39ceb7e4e4fd3b958a
Andrew Nzai <andrewnzaikombe@gmail.com>
2024-04-24 06:28:14 +0300

    Added submission display

--- frontend/src/pages/Student/Assignments/Completed/Completed.tsx
+++ frontend/src/pages/Student/Assignments/Completed/Completed.tsx
@@ -1,10 +1,71 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Completed.css";
+import SidebarImpl from "../../../../components/Sidebar/SidebarImpl";
+import { useNavigate } from "react-router-dom";
+import Loader from "../../../../components/Loader/Loader";
+import AuthService from "../../../../services/Auth.service";
+import SubmissionService from "../../../../services/Submission.service";
+import { Submission } from "../../../../models/Submission";
+
 
 function Completed(){
+    const authService = new AuthService();
+    const submissionService = new SubmissionService();
+
+    const [loading, setLoading] = useState(true);
+    const [submissions, setSubmissions] = useState<Submission[]>([]);
+
+    const navigate = useNavigate();
+
+    useEffect(
+        () => {
+      
+            const currentUser: any | null = authService.getCurrentUser();
+      
+      if(!currentUser){
+        navigate("/login");
+      }
+
+      setTimeout(
+        () => {
+          submissionService.getAllSubmission()
+          .then(
+            (response) => {
+                setSubmissions(response);
+            }, (error) => {
+                alert("No submissions");
+                navigate("/lecturerDashboard");
+            }
+          );
+        }
+      , 3000);
+        }
+        
+        ,[]);
+
+        if(loading){
+            return(
+              <div className="container">
+                <Loader/>
+              </div>
+            );
+          }
+
     return(
         <div>
-            <p>Completed</p>
+            <SidebarImpl/>
+            <div className="submission-list">
+      {submissions.map((detail, index) => (
+        <div key={index} className="submission-item">
+          <p><strong>Title:</strong> {detail.assignmentTitle}</p>
+          <p><strong>Registration:</strong> {detail.studentRegistration}</p>
+          <p><strong>Unit Code:</strong> {detail.unitCode}</p>
+          <p><strong>Score:</strong> {detail.score}</p>
+          <p><strong>Feedback:</strong> {detail.feedback}</p>
+          <p><strong>Accepted:</strong> {detail.accepted ? "Yes" : "No"}</p>
+        </div>
+      ))}
+    </div>
         </div>
     );
 }
