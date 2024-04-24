e003f7c7aebd8885c915e9853ff42dac17278b04
Andrew Nzai <andrewnzaikombe@gmail.com>
2024-04-24 10:32:11 +0300

    Styling

--- frontend/src/pages/Admin/Dashboard/Dashboard.tsx
+++ frontend/src/pages/Admin/Dashboard/Dashboard.tsx
@@ -1,12 +1,16 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import AdAuthService from "../../../services/AdAuth.service";
 import { useNavigate } from "react-router-dom";
 import AdSidebar from "../../../components/Sidebar/AdSidebar";
 import "./Dashboard.css";
+import Loader from "../../../components/Loader/Loader";
 
 function AdDashboard(){
     const authService = new AdAuthService();
     const navigate = useNavigate();
+
+    const [loading, setLoading] = useState(true);
+  
     
     useEffect(() => {
       const currentUser: any | null = authService.getCurrentUser();
@@ -14,8 +18,21 @@ function AdDashboard(){
       if(!currentUser){
         navigate("/login");
       }
+      setTimeout(
+        () => {
+          
+        }
+      , 3000);
 
     }, [navigate]);
+
+    if(loading){
+      return(
+        <div className="container">
+          <Loader/>
+        </div>
+      );
+    }
 
     return (
         <div>
