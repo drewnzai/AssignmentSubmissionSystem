97c4a691f206bb9a42d8d9369a4d9d74dc42fce6
Andrew Nzai <andrewnzaikombe@gmail.com>
2024-04-30 20:27:01 +0300

    Rectified path

--- frontend/src/services/LecSubmission.service.ts
+++ frontend/src/services/LecSubmission.service.ts
@@ -32,7 +32,7 @@ export default class LecSubmissionService{
     }
 
     modifySubmission(submission: Submission){
-        return axios.post(API_URL + "/modify", submission, {headers: LecAuthHeader()})
+        return axios.post(API_URL + "/" + "update", submission, {headers: LecAuthHeader()})
         .then(
             (response) => {
                 if(response.data)
