490594872990f34c3d5c0b14f2f1e09cbb358e9e
Andrew Nzai <andrewnzaikombe@gmail.com>
2024-04-30 19:52:54 +0300

    Get file from s3 bucket

---
+++ Backend/Admin/src/main/java/com/andrew/AssignmentSubmission/api/FileDownloaderController.java
@@ -0,0 +1,23 @@
+package com.andrew.AssignmentSubmission.api;
+
+import com.andrew.AssignmentSubmission.dto.MiscRequest;
+import com.andrew.AssignmentSubmission.services.AmazonService;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.AllArgsConstructor;
+import org.springframework.core.io.Resource;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/download")
+@AllArgsConstructor
+@Tag(name = "File Download", description = "Retrieve File from AWS S3 Bucket")
+public class FileDownloaderController {
+
+    private AmazonService amazonService;
+
+    @PostMapping()
+    public Resource downloadFile(@RequestBody MiscRequest miscRequest) {
+        return amazonService.download(miscRequest.getData());
+    }
+
+}
---
+++ Backend/Admin/src/main/java/com/andrew/AssignmentSubmission/services/AmazonService.java
@@ -0,0 +1,28 @@
+package com.andrew.AssignmentSubmission.services;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.S3Object;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class AmazonService {
+
+    @Value("${aws.bucket.name}")
+    private String bucketName;
+
+    private final AmazonS3 amazonS3;
+
+    public Resource download(String path){
+        try {
+            S3Object s3Object = amazonS3.getObject(bucketName, path);
+            return new UrlResource(s3Object.getObjectContent().getHttpRequest().getURI().toURL());
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to download file", e);
+        }
+    }
+}
