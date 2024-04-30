eea0e27c422fec333736a0216b01c10e6ebffaa8
Andrew Nzai <andrewnzaikombe@gmail.com>
2024-04-30 17:50:05 +0300

    Optimised imports

--- frontend/src/components/Sidebar/LecSidebar.tsx
+++ frontend/src/components/Sidebar/LecSidebar.tsx
@@ -1,27 +1,20 @@
 import { useEffect, useState } from "react";
 
-import AuthService from "../../services/Auth.service";
-import "./SidebarImpl.css";
-import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
-import AssignmentLateIcon from '@mui/icons-material/AssignmentLate';
+import AddIcon from '@mui/icons-material/Add';
 import AssignmentIcon from '@mui/icons-material/Assignment';
+import AssignmentIndIcon from '@mui/icons-material/AssignmentInd';
 import BookIcon from '@mui/icons-material/Book';
-import CalendarTodayOutlinedIcon from "@mui/icons-material/CalendarTodayOutlined";
-import CheckIcon from '@mui/icons-material/Check';
+import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
 import HelpOutlineOutlinedIcon from "@mui/icons-material/HelpOutlineOutlined";
 import HomeIcon from '@mui/icons-material/Home';
 import LogoutIcon from '@mui/icons-material/Logout';
 import MenuRoundedIcon from "@mui/icons-material/MenuRounded";
 import PersonIcon from '@mui/icons-material/Person';
-import AddIcon from '@mui/icons-material/Add';
-import AssignmentIndIcon from '@mui/icons-material/AssignmentInd';
-import SchoolIcon from '@mui/icons-material/School';
-import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
-import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 import { Menu, MenuItem, Sidebar, SubMenu } from 'react-pro-sidebar';
 import { Link, useNavigate } from "react-router-dom";
 import { Unit } from "../../models/Unit";
 import LecAuthService from "../../services/LecAuth.service";
+import "./SidebarImpl.css";
 
 function LecSidebar(){
   const [collapsed, setCollapsed] = useState(true);
