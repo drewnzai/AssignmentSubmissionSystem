39b5c830ffceea834afc4e0ffa184e51877263e3
Andrew Nzai <andrewnzaikombe@gmail.com>
2024-05-11 19:59:35 +0300

    Created Tabs

--- Frontend/src/components/Admin/StudentManagement.tsx
+++ Frontend/src/components/Admin/StudentManagement.tsx
@@ -1,144 +1,91 @@
-import {Box, Button, InputLabel, MenuItem, Select, TextField, useMediaQuery} from "@mui/material";
-import {Formik} from "formik";
-import * as yup from "yup";
-import { StudentDto } from "../../models/StudentDto";
-import AdminService from "../../services/Admin.service";
-import { useEffect, useState } from "react";
-import { Course } from "../../models/Course";
+import {Box} from "@mui/material";
+import {styled} from '@mui/system';
+import {TabsList as BaseTabsList} from '@mui/base/TabsList';
+import {TabPanel as BaseTabPanel} from '@mui/base/TabPanel';
+import {buttonClasses} from '@mui/base/Button';
+import {Tab as BaseTab, tabClasses} from '@mui/base/Tab';
+import {Tabs} from '@mui/base/Tabs';
+import DeleteStudent from "./DeleteStudent";
+import AddStudent from "./AddStudent";
+import {tokens} from "../../theme";
+import useTheme from "@mui/material/styles/useTheme";
 
-const registrationRegEx = new RegExp("[A-Z]|[A-Z]P\d{2}/\d{5}/\d{2}");
-
-const checkoutSchema = yup.object().shape({
-    firstName: yup.string().required("required"),
-    lastName: yup.string().required("required"),
-    registration: yup.string()
-    .matches(registrationRegEx, "Invalid Registration Number")
-    .required("required")
-});
-
-const initialValues:StudentDto = {
-  firstName: "",
-  lastName: "",
-  registration: "",
-  courseName: ""
-};
 
 export default function StudentManagement(){
-  
-  const isNonMobile = useMediaQuery("(min-width:600px)");
 
-  const [courses, setCourses] = useState<Course[]>([]);
-  
-  const service =  new AdminService();
-  
-  const handleFormSubmit = (values: StudentDto) => {
-        
-        service.addStudent(values);
-    };
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
 
-    useEffect(
-      () => {
-        service.getCourses()
-        .then(
-          (response: Course[]) => {
-            setCourses(response);
-          }
-        )
-      }, [])
+  
+  
+  const Tab = styled(BaseTab)`
+  font-family: 'Source Sans 3', sans-serif;
+  color: white;
+  cursor: pointer;
+  font-size: 0.875rem;
+  font-weight: bold;
+  background-color: transparent;
+  width: 100%;
+  line-height: 1.5;
+  padding: 8px 12px;
+  margin: 6px;
+  border: none;
+  border-radius: 8px;
+  display: flex;
+  justify-content: center;
+
+  &:hover {
+    background-color: ${colors.blueAccent[400]};
+  }
+
+  &:focus {
+    color: #fff;
+    outline: 3px solid ${colors.blueAccent[200]};
+  }
+
+  &.${tabClasses.selected} {
+    background-color: #fff;
+    color: ${colors.blueAccent[600]};
+  }
+
+  &.${buttonClasses.disabled} {
+    opacity: 0.5;
+    cursor: not-allowed;
+  }
+`;
+
+const TabPanel = styled(BaseTabPanel)`
+  width: 100%;
+  font-family: 'IBM Plex Sans', sans-serif;
+  font-size: 0.875rem;
+`;
+
+const TabsList = styled(BaseTabsList)(
+  ({ theme }) => `
+  min-width: 400px;
+  background-color: ${colors.blueAccent[500]};
+  border-radius: 12px;
+  margin-bottom: 16px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  align-content: space-between;
+  box-shadow: 0px 4px 6px ${
+    theme.palette.mode === 'dark' ? 'rgba(0,0,0, 0.4)' : 'rgba(0,0,0, 0.2)'
+  };
+  `)
 
     
     return(
         <Box m="20px">
-        <Formik
-        onSubmit={handleFormSubmit}
-        initialValues={initialValues}
-        validationSchema={checkoutSchema}
-      >
-        {({
-          values,
-          errors,
-          touched,
-          handleBlur,
-          handleChange,
-          handleSubmit,
-        }) => (
-          <form onSubmit={handleSubmit}>
-            <Box
-              display="grid"
-              gap="30px"
-              gridTemplateColumns="repeat(4, minmax(0, 1fr))"
-              sx={{
-                "& > div": { gridColumn: isNonMobile ? undefined : "span 4" },
-              }}
-            >
-              <TextField
-                fullWidth
-                variant="filled"
-                type="text"
-                label="First Name"
-                onBlur={handleBlur}
-                onChange={handleChange}
-                value={values.firstName}
-                name="firstName"
-                error={!!touched.firstName && !!errors.firstName}
-                helperText={touched.firstName && errors.firstName}
-                sx={{ gridColumn: "span 2" }}
-              />
-
-              <TextField
-                fullWidth
-                variant="filled"
-                type="text"
-                label="Last Name"
-                onBlur={handleBlur}
-                onChange={handleChange}
-                value={values.lastName}
-                name="lastName"
-                error={!!touched.lastName && !!errors.lastName}
-                helperText={touched.lastName && errors.lastName}
-                sx={{ gridColumn: "span 2" }}
-              />
-             
-              <TextField
-                fullWidth
-                variant="filled"
-                type="text"
-                label="Registration"
-                onBlur={handleBlur}
-                onChange={handleChange}
-                value={values.registration}
-                name="registration"
-                error={!!touched.registration && !!errors.registration}
-                helperText={touched.registration && errors.registration}
-                sx={{ gridColumn: "span 2" }}
-              />
-
-          <InputLabel id="demo-simple-select-label">Course Name</InputLabel>
-            <Select
-              labelId="demo-simple-select-label"
-              id="demo-simple-select"
-              value={values.courseName}
-              label="Age"
-              name="courseName"
-              onChange={handleChange}
-            >
-              {courses.map(
-                (course) => (
-                  <MenuItem value={course.name}>{course.name}</MenuItem>
-                )
-            
-            )}
-            </Select>
-              
-            </Box>
-            <Box display="flex" justifyContent="space-between" mt="20px">
-            <Button type="submit" color="secondary" variant="contained">
-                Create New Student
-              </Button>
-            </Box>
-          </form>
-        )}
-      </Formik>
+        <Tabs defaultValue={0}>
+      <TabsList>
+        <Tab value={0}>Add</Tab>
+        <Tab value={1}>Delete</Tab>
+      </TabsList>
+      <TabPanel value={0}><AddStudent/></TabPanel>
+      <TabPanel value={1}><DeleteStudent/></TabPanel>
+      </Tabs>
         </Box>
     );
 }
