a4d092b47d23fdd3ab0a9d67a7cc6bbb4c779d88
Andrew Nzai <andrewnzaikombe@gmail.com>
2024-05-17 17:49:39 +0300

    Added Lecturer to conditional

--- Frontend/src/pages/Redirect.tsx
+++ Frontend/src/pages/Redirect.tsx
@@ -1,18 +1,25 @@
 import { useNavigate } from "react-router-dom";
 import AdminService from "../services/Admin.service";
 import { useEffect } from "react";
+import LecturerService from "../services/Lecturer.service";
 
 export default function Redirect(){
     const adminService = new AdminService();
+    const lecturerService = new LecturerService();
 
     const navigate = useNavigate();
 
     const admin = adminService.getCurrentUser();
+    const lecturer = lecturerService.getCurrentUser();
 
     useEffect(() => {
         if(admin){
             navigate("/admin/home");
-        }else{
+        }
+        else if(lecturer){
+            navigate("/lecturer/home");
+        }
+        else{
             navigate("/login")
         }
     }, []);
