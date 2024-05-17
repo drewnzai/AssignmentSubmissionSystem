2815ee0030de5110818e589cd368c85ffc835ffc
Andrew Nzai <andrewnzaikombe@gmail.com>
2024-05-17 17:55:31 +0300

    Navigate to admin home

--- Frontend/src/components/Admin/AdminLogin.tsx
+++ Frontend/src/components/Admin/AdminLogin.tsx
@@ -32,7 +32,7 @@ export default function AdminLogin(){
         service.login(values)
         .then(
             (_response) => {
-                navigate("/");
+                navigate("/admin/home");
             }
         )
         
