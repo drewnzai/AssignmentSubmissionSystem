96f42b322281ca16c35152a68ed1d028282ed0ea
Andrew Nzai <andrewnzaikombe@gmail.com>
2024-05-18 17:51:48 +0300

    Modified Lecturer Sidebar

--- Frontend/src/components/Lecturer/Sidebar.tsx
+++ Frontend/src/components/Lecturer/Sidebar.tsx
@@ -8,7 +8,7 @@ import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
 import LogoutIcon from '@mui/icons-material/Logout';
 import AssignmentIndIcon from '@mui/icons-material/AssignmentInd';
 import AssignmentIcon from '@mui/icons-material/Assignment';
-// import HelpOutlineOutlinedIcon from "@mui/icons-material/HelpOutlineOutlined";
+import HelpOutlineOutlinedIcon from "@mui/icons-material/HelpOutlineOutlined";
 import MenuOutlinedIcon from "@mui/icons-material/MenuOutlined";
 import LecturerService from "../../services/Lecturer.service";
 
@@ -145,7 +145,13 @@ export default function Sidebar(){
                 selected={selected}
                 setSelected={setSelected}
               />
-              
+              <Item
+                title="FAQ Page"
+                to="/lecturer/faq"
+                icon={<HelpOutlineOutlinedIcon />}
+                selected={selected}
+                setSelected={setSelected}
+              />
               <MenuItem
               style={{
                 color: colours.grey[100],
