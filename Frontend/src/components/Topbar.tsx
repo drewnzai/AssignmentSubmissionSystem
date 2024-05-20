4642b22fa09a1f97b42423187f60aa4e538261b1
Andrew Nzai <andrewnzaikombe@gmail.com>
2024-05-20 18:49:57 +0300

    Topbar can only change the mode

--- Frontend/src/components/Topbar.tsx
+++ Frontend/src/components/Topbar.tsx
@@ -1,51 +1,25 @@
-import {Box, IconButton, InputBase, useTheme} from "@mui/material";
+import {Box, IconButton, useTheme} from "@mui/material";
 import {useContext} from "react";
 import {ColorModeContext, tokens} from "../theme";
 import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
 import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
-import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined";
-import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
-import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
-import SearchIcon from "@mui/icons-material/Search";
 
 
 export default function Topbar(){
     const theme = useTheme();
-    const colours = tokens(theme.palette.mode);
     const colourMode = useContext(ColorModeContext);
 
 
     return(
-        <Box display="flex" justifyContent="space-between" p={2}>
-        
-        <Box sx={{
-            display: "flex",
-            backgroundColor: colours.primary[400],
-            borderRadius: "3px"
-        }}>
-          <InputBase sx={{ ml: 2, flex: 1 }} placeholder="Search" />
-          <IconButton type="button" sx={{ p: 1 }}>
-            <SearchIcon />
-          </IconButton>
-        </Box>
-  
-        
-        <Box display="flex">
+        <Box display="flex" p={2}>
+       
+        <Box display="flex" >
           <IconButton onClick={colourMode.toggleColorMode}>
             {theme.palette.mode === "dark" ? (
               <LightModeOutlinedIcon />
             ) : (
                 <DarkModeOutlinedIcon />
             )}
-          </IconButton>
-          <IconButton>
-            <NotificationsOutlinedIcon />
-          </IconButton>
-          <IconButton>
-            <SettingsOutlinedIcon />
-          </IconButton>
-          <IconButton>
-            <PersonOutlinedIcon />
           </IconButton>
         </Box>
       </Box>
